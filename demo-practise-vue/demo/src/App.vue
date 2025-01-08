1c3becdafdaa65e224d5617c6e15255c355cce67
david <25137527@qq.com>
2025-01-08 18:11:27 +0800

    commit

--- demo-practise-vue/demo/src/App.vue
+++ demo-practise-vue/demo/src/App.vue
@@ -1,45 +1,48 @@
-<!-- <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
 <template>
   <div>
-    <button @click="fetchData">Fetch Data</button>
+    <!-- <button @click="fetchData">Fetch Data</button> -->
+    <input v-model="loginForm.username" placeholder="账号" />
+    <p>输入的内容是: {{ loginForm.username }}</p>
+
+    <input v-model="loginForm.password" placeholder="密码" />
+    <p>输入的内容是: {{ loginForm.password }}</p>
+    <button @click="handleLogin">登录</button>
   </div>
 </template>
- 
+
 <script>
 import axios from 'axios';
- 
+
 export default {
   name: 'MyComponent',
+  data() {
+    return {
+      loginForm: {
+        username: "admin",
+        password: "admin123",
+        rememberMe: false,
+        code: "",
+        uuid: ""
+      },
+    }
+  },
   methods: {
-    fetchData() {
-      axios.get('/api/data')
+    // fetchData() {
+    //   axios.get('/api/data')
+    //     .then(response => {
+    //       console.log(response.data);
+    //     })
+    //     .catch(error => {
+    //       console.error(error);
+    //     });
+    // }
+    handleLogin() {
+      axios.get('/api/login', {
+        params: {
+          name: this.loginForm.username,
+          password: this.loginForm.password
+        }
+      })
         .then(response => {
           console.log(response.data);
         })
